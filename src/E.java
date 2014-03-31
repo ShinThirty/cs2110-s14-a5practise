ff69515548ccf74dfdb729a4ade71ee9d607f995
ShinThirty <xmxu00@gmail.com>
2014-03-31 18:44:15 -0400

    String part Finished.

--- src/E.java
+++ src/E.java
@@ -12,30 +12,77 @@ public class E {
 	/** Returns a copy of parameter s with all instances of 'd'
 	 * duplicated. */
 	public static String dupD(String s) {
-		String dupS = "";
-		int indexD = 0;
-		int startIndex = 0;
-		while (startIndex < s.length()) {
-			indexD = s.indexOf('d', startIndex);
-			if (indexD >= 0) {
-				dupS = dupS + s.substring(startIndex, indexD + 1) + 'd';
-				startIndex = indexD + 1;
-			}
-			else {
-				dupS = dupS + s.substring(startIndex);
-				break;
-			}
-		}
-		return dupS;
+		return s.replace("d", "dd");
 	}
 	
-	/** Returns a word sequence with fixed capitalization and spaces. */
+	/** Returns a word sequence with fixed capitalization and spaces. 
+	 * (1) No blanks at the beginning and end;
+	 * (2) Exactly one blank between adjacent words;
+	 * (3) All words capitalized. */
 	public static String fixCap(String s) {
-		int startIndex = 0;
 		String fixedS = "";
-		while (startIndex < s.length()) {
-			
+		int indexWS = 0;
+		String word = "";
+		while (s != "") {
+			s = s.trim();
+			indexWS = s.indexOf(' ');
+			if (indexWS >= 0) {
+				word = s.substring(0, indexWS);
+				word = Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
+				fixedS = fixedS + word + ' ';
+				s = s.substring(indexWS);
+			}
+			else {
+				word = Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
+				fixedS = fixedS + word;
+				s = "";
+			}
 		}
+		return fixedS;
+	}
+	
+	/** Returns a string with the same names as the input string with the format of every
+	 * name being "last, first" and with only 1 blank after each semicolon. */
+	public static String fixNames(String s) {
+		String fixedNames = "";
+		int indexSC = 0;
+		String[] name = {"", ""};
 		
+		while (s != "") {
+			s = s.trim();
+			indexSC = s.indexOf(';');
+			if (indexSC >= 0) {
+				name = s.substring(0, indexSC).split(" ");
+				fixedNames = fixedNames + name[1] + ", " + name[0] + "; ";
+				s = s.substring(indexSC + 1);
+			}
+			else {
+				name = s.split(" ");
+				fixedNames = fixedNames + name[1] + ", " + name[0];
+				s = "";
+			}
+		}
+		return fixedNames;
+	}
+	
+	/** Returns the equivalent 12-hour form of a time of day in 24-hour time. 
+	 * Precondition: hour >= 0 and hour < 24;
+	 *               min  >= 0 and min  < 60 */
+	public static String make12hr(String s) {
+		String[] time = {"", ""};
+		time = s.split(":");
+		int hour = Integer.parseInt(time[0]);
+		int min = Integer.parseInt(time[1]);
+		
+		assert hour >= 0 && hour < 24 && min >= 0 && min < 60;
+		
+		int hour12 = (hour >= 12) ? (hour - 12) : hour;
+		return String.format("%02d:%02d%s", hour12, min, (hour >= 12) ? "PM" : "AM");
+	}
+	
+	/** Return its parameter String s with "_" inserted before and after each 
+	 * occurrence of 'a'. */
+	public static String fixA(String s) {
+		return s.replace("a", "_a_");
 	}
 }
--- src/ETester.java
+++ src/ETester.java
@@ -29,5 +29,49 @@ public class ETester {
 		dupS = E.dupD("dd00000");
 		assertEquals("dddd00000", dupS);
 	}
+	
+	@Test
+	public void fixCapTest() {
+		String wordSequence = "  where  '\u0131'   is   the LATIN  SMALL LETTER DOTLESS    I character ";
+		String fixedWS = E.fixCap(wordSequence);
+		assertEquals("Where '\u0131' Is The Latin Small Letter Dotless I Character", fixedWS);
+		wordSequence = "  wHERE  ";
+		fixedWS = E.fixCap(wordSequence);
+		assertEquals("Where", fixedWS);
+		wordSequence = "";
+		fixedWS = E.fixCap(wordSequence);
+		assertEquals("", fixedWS);
+	}
+	
+	@Test
+	public void fixNamesTest() {
+		String nameSequence = " David Gries;    Ashutosh Saxena;  Haym Hirsh  ";
+		String fixedNames = E.fixNames(nameSequence);
+		assertEquals("Gries, David; Saxena, Ashutosh; Hirsh, Haym", fixedNames);
+	}
+	
+	@Test
+	public void make12hrTest() {
+		String[] cases = {"0:5", "1:20", "12:50", "23:59"};
+		String time12hr = "";
+		String[] expectedResults = {"00:05AM", "01:20AM", "00:50PM", "11:59PM"};
+		
+		for (int i = 0; i < cases.length; i++) {
+			time12hr = E.make12hr(cases[i]);
+			assertEquals(expectedResults[i], time12hr);
+		}
+	}
+	
+	@Test
+	public void fixATest() {
+		String[] cases = {"anna", "0000", "aaaa"};
+		String fixedA = "";
+		String[] expectedResults = {"_a_nn_a_", "0000", "_a__a__a__a_"};
+		
+		for (int i = 0; i < cases.length; i++) {
+			fixedA = E.fixA(cases[i]);
+			assertEquals(expectedResults[i], fixedA);
+		}
+	}
 
 }

fdbe212aa018c0b8056047722b5c47840200fc1d
ShinThirty <xmxu00@gmail.com>
2014-04-11 14:41:11 -0400

    Template downloaded. Starting reviewing.

--- src/E.java
+++ src/E.java
@@ -1,88 +1,39 @@
-/** This class contains the exercising methods about String. */
+
 public class E {
-	/** Returns the number of times character c appears in string s. */
-	public static int number(char c, String s) {
-		int number = 0;
-		for (int i = 0; i < s.length(); i++)
-			if (s.charAt(i) == c)
-				number++;
-		return number;
-	}
-	
-	/** Returns a copy of parameter s with all instances of 'd'
-	 * duplicated. */
-	public static String dupD(String s) {
-		return s.replace("d", "dd");
-	}
-	
-	/** Returns a word sequence with fixed capitalization and spaces. 
-	 * (1) No blanks at the beginning and end;
-	 * (2) Exactly one blank between adjacent words;
-	 * (3) All words capitalized. */
-	public static String fixCap(String s) {
-		String fixedS = "";
-		int indexWS = 0;
-		String word = "";
-		while (s.equals("") == false) {
-			s = s.trim();
-			indexWS = s.indexOf(' ');
-			if (indexWS >= 0) {
-				word = s.substring(0, indexWS);
-				word = Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
-				fixedS = fixedS + word + ' ';
-				s = s.substring(indexWS);
-			}
-			else {
-				word = Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
-				fixedS = fixedS + word;
-				s = "";
-			}
-		}
-		return fixedS;
-	}
-	
-	/** Returns a string with the same names as the input string with the format of every
-	 * name being "last, first" and with only 1 blank after each semicolon. */
-	public static String fixNames(String s) {
-		String fixedNames = "";
-		int indexSC = 0;
-		String[] name = {"", ""};
-		
-		while (s.equals("") == false) {
-			s = s.trim();
-			indexSC = s.indexOf(';');
-			if (indexSC >= 0) {
-				name = s.substring(0, indexSC).split(" ");
-				fixedNames = fixedNames + name[1] + ", " + name[0] + "; ";
-				s = s.substring(indexSC + 1);
-			}
-			else {
-				name = s.split(" ");
-				fixedNames = fixedNames + name[1] + ", " + name[0];
-				s = "";
-			}
-		}
-		return fixedNames;
-	}
-	
-	/** Returns the equivalent 12-hour form of a time of day in 24-hour time. 
-	 * Precondition: hour >= 0 and hour < 24;
-	 *               min  >= 0 and min  < 60 */
-	public static String make12hr(String s) {
-		String[] time = {"", ""};
-		time = s.split(":");
-		int hour = Integer.parseInt(time[0]);
-		int min = Integer.parseInt(time[1]);
-		
-		assert hour >= 0 && hour < 24 && min >= 0 && min < 60;
-		
-		int hour12 = (hour >= 12) ? (hour - 12) : hour;
-		return String.format("%02d:%02d%s", hour12, min, (hour >= 12) ? "PM" : "AM");
-	}
-	
-	/** Return its parameter String s with "_" inserted before and after each 
-	 * occurrence of 'a'. */
-	public static String fixA(String s) {
-		return s.replace("a", "_a_");
-	}
+
+    /** String-loop question 1 
+     * Return the number of occurrences of c in s */
+    public static int occurrences(char c, String s) {
+        return 0;
+    }
+    
+    
+    /* String-loop question 2 
+     * Below, write the spec and the method dupD */
+    
+    
+    
+    
+    /* String-loop question 3 
+     * Below, write the spec and the method fixCap */
+    
+    
+    
+    /* String-loop question 4 
+     * Below, write the spec and the method fixNames  */
+    
+    
+    
+    /* String-loop question 5 
+     * Below, write the spec and the method make12hr   */
+    
+
+ 
+    /* String-loop question 6 
+     * Below, write the spec and the method fixA    */
+
+    
+    
+   
+
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,172 +1,115 @@
 import java.util.*;
 
-/** This class contains the exercising methods about recursion. */
-public class TreeNode<T> {
-	private T datum; // The data stored in tree node of type T
-	private TreeNode<T> left, right; // The left and right child node of this node
-	
-	/** Constructor: one node tree with datum x. */
-	public TreeNode(T x) {
-		datum = x;
-	}
-	
-	/** Constructor: Tree node with root value x, left tree lft and right tree rgt. */
-	public TreeNode(T x, TreeNode<T> lft, TreeNode<T> rgt) {
-		datum = x;
-		left = lft;
-		right = rgt;
-	}
-	
-	/** Return the datum of this node. */
-	public T getDatum() {
-		return datum;
-	}
-	
-	/** Set the datum of this node. */
-	public void setDatum(T x) {
-		datum = x;
-	}
-	
-	/** Return the left child node. */
-	public TreeNode<T> getLeft() {
-		return left;
-	}
-	
-	/** Set the left child node of this node. */
-	public void setLeft(TreeNode<T> lft) {
-		left = lft;
-	}
-	
-	/** Return the right child node. */
-	public TreeNode<T> getRight() {
-		return right;
-	}
-	
-	/** Set the right child node of this node. */
-	public void setRight(TreeNode<T> rgt) {
-		right = rgt;
-	}
-	
-	/** Return the number of nodes of this binary tree with 
-	 * its root to be this node. */
-	public int getNumOfNodes() {
-		return ((left == null) ? 0 : left.getNumOfNodes()) + 
-			   ((right == null)? 0 : right.getNumOfNodes()) +
-			   1;
-	}
-	
-	/** Return the number of leaves of this binary tree with
-	 * its root to be this node. */
-	public int getNumOfLeaves() {
-		return (left == null && right == null) ? 1 :
-			   (left == null) ? right.getNumOfLeaves() :
-			   (right == null)? left.getNumOfLeaves() :
-			   left.getNumOfLeaves() + right.getNumOfLeaves();
-	}
-	
-	/** Return the result of b**c in O(c) time. 
-	 * Precondition: c >= 0. */
-	public static double bPowerc(double b, int c) {
-		assert c >= 0;
-		
-		return (c == 0) ? 1 :
-			   (c % 2 == 1) ? b * bPowerc(b, c-1) :
-				              bPowerc(b*b, c/2);
-	}
-	
-	/** Return true if all the letters in String x are included in 
-	 * String s and in the same order. */
-	public static boolean isXish(String x, String s) {
-		return x.equals("") ? true :
-			s.indexOf(x.charAt(0)) >= 0 &&
-			isXish(x.substring(1), s.substring(s.indexOf(x.charAt(0))+1));
-	}
-	
-	/** Return true if the given word String s contains the letters e, 
-	 * l and f without considering the order. */
-	public static boolean isElfish(String s) {
-		return s.contains("e") && s.contains("l") && s.contains("f");
-	}
-	
-	/** Return a Set<String> that contains all permutations of s of 
-	 * all-different characters. */
-	public static Set<String> permutations(String s) {
-		if (s.length() == 0)
-			return new HashSet<String>();
-		else if (s.length() == 1) {
-			Set<String> permutationSet = new HashSet<String>();
-			permutationSet.add(s);
-			return permutationSet;
-		}
-		else {
-			Set<String> permutationSetLesser = permutations(s.substring(1));
-			Set<String> permutationSet = new HashSet<String>();
-			for (String permutationLesser: permutationSetLesser)
-				for (int offset = 0; offset <= permutationLesser.length(); offset++)
-					permutationSet.add(new StringBuilder(permutationLesser).insert(offset, s.charAt(0)).toString());
-			return permutationSet;
-		}
-	}
-	
-	public enum Player {
-		Alice, Bob;
-		
-		/** Return the opponent of this player. */
-		public Player opponent() {
-			return this == Alice ? Bob : Alice;
-		}
-		
-		/** Return the player as a String. */
-		public String toString() {
-			return this == Alice ? "Alice" : "Bob";
-		}
-	}
-	
-	public static class PickCoinResult {
-		private Player winner;
-		private int NumOfStrategies;
-		
-		/** Constructor. */
-		public PickCoinResult(Player win, int NOS) {
-			winner = win;
-			NumOfStrategies = NOS;
-		}
-		
-		/** Get the value of fields. */
-		public Player getWinner() {
-			return winner;
-		}
-		
-		public int getNOS() {
-			return NumOfStrategies;
-		}
-	}
-	
-	/** Return the winner of the pick coin game and the number of strategies. */
-	public static PickCoinResult pickCoin(int NumOfCoins, Player first, Player second) {
-		assert NumOfCoins > 0;
-		
-		if (NumOfCoins == 1 || NumOfCoins == 2)
-			return new PickCoinResult(first, 1);
-		else if (NumOfCoins == 3)
-			return new PickCoinResult(second, 2);
-		else if (NumOfCoins == 4)
-			return new PickCoinResult(first, 3);
-		else {
-			// NumOfCoins > 4
-			if (NumOfCoins % 3 == 0) {
-				int NumOfStrategies = pickCoin(NumOfCoins - 1, second, first).getNOS() + 
-									  pickCoin(NumOfCoins - 2, second, first).getNOS() + 
-									  pickCoin(NumOfCoins - 4, second, first).getNOS();
-				return new PickCoinResult(second, NumOfStrategies);
-			}
-			else {
-				int NumOfStrategies = ((NumOfCoins - 1) % 3 == 0) ? 
-					   (pickCoin(NumOfCoins - 1, second, first).getNOS() + 
-						pickCoin(NumOfCoins - 4, second, first).getNOS()) : 
-						pickCoin(NumOfCoins - 2, second, first).getNOS();
-				return new PickCoinResult(first, NumOfStrategies);
-			}
-		}
-	}
+/** An instance is a node in a binary tree; it may be viewed also as the root of a tree */
+public class TreeNode {
+    private int val;  // The value in the node
+    private TreeNode left; // The left subtree (null if empty)
+    private TreeNode right; // The right subtree (null if empty)
+    
+    
+    // Recursion problem 1. Write the bodies of the following two methods size
+    
+    /** Return the size of the tree with this as the root. */
+    public int size() {
+        // complete this method
+        return 0;
+    }
+    
+    /** Return the size of the tree whose root is r. */
+    public static int size(TreeNode r) {
+        // complete this method
+        return 0;
+    }
+    
+    
+    // Recursion problem 2. Problem 1 illustrates how one could have a static and a
+    // non-static method do essentially the same thing. Below, write
+    // static and non-static functions that return the number of leaves of a tree.
+    // Write the specifications of the methods as javadoc comments before writing
+    // the method bodies.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Recursion problem 3. Write a recursive function to calculate  b to the power c,
+    // as described in the A5 handout. Be sure to specify it first.
+    
+    
+    
+    
+    
+    
+    // Recursion problem 4. write functions isXish and isElfish as described in the
+    // hA5 handout. Be sure to specify them first.
+    
+    
+    
+    
+    // Problem 5. Write the following function permutations. See the A5 handout
+    // for hints.
+    
+    /** Return a set of all permutations of s.
+     *  Precondition: s contains no duplicates --all chars are different*/
+    public static Set<String> permutations(String s) {
+        
+        
+        return null;
+    }
+    
+    
+    
+    // Write your coin-game stuff here (it is optional)
+    
+    
+    
+    
+
+    /** Constructor: a one-node tree with value v */
+    public TreeNode(int v) {
+        val= v;
+    }
+
+    /**Constructor: a treee with value v, left subtree left, and right subtee right */
+    public TreeNode(int v, TreeNode left, TreeNode right) {
+        val= v;
+        this.left= left;
+        this.right= right;
+    }
+
+
+    /** a representation of this tree. The root appears on one line.
+        then its left subtree indented 2 spaces in the same representation (null if none)
+        then its right subtree similarly.
+        If the tree is a leaf, its subtrees do not appear. */
+    public @Override String toString() {
+        return toString("");
+    }
+
+    /** Like toString() except that everything is indented by sp (assumed to be 
+     * a string with blanks in it) */
+    public String toString(String sp) {
+        if (left == null  && right == null) {
+            return sp + val;
+        }
+        return sp + val +
+               "\n" + (left == null ? sp + "  " + null : left.toString(sp + "  ")) +
+               "\n" + (right == null ? sp + "  " + null : right.toString(sp + "  "));
+    }
+    
+    /** = a tree  (1 2 (3 (4 5 6) null)) */
+    public static TreeNode tree1() {
+        TreeNode t1= new TreeNode(1);
+        TreeNode t2= new TreeNode(2);
+        TreeNode t3= new TreeNode(3);
+        TreeNode t4= new TreeNode(4);
+        TreeNode t5= new TreeNode(5);
+        TreeNode t6= new TreeNode(6);
+        return new TreeNode(1, t2, new TreeNode(3, new TreeNode(4, t5, t6), null));
+    }
+
 }
